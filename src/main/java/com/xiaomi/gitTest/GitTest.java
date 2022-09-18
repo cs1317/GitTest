12cf95dca11f7f8a0eded1d77e5a9c00b9921046
chengsai1 <chengsai1@xiaomi.com>
2022-09-18 20:29:13 +0800

    hot-fix commit1

--- src/main/java/com/xiaomi/gitTest/GitTest.java
+++ src/main/java/com/xiaomi/gitTest/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("fgg");
         System.out.println("git change1");
+        System.out.println("hello hot-fix");
     }
 }
