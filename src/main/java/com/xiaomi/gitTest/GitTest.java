91ab980236879e7e444734dd4899feace0921fb2
chengsai1 <chengsai1@xiaomi.com>
2022-09-18 20:18:14 +0800

    Idea second commit

--- src/main/java/com/xiaomi/gitTest/GitTest.java
+++ src/main/java/com/xiaomi/gitTest/GitTest.java
@@ -3,5 +3,6 @@ package com.xiaomi.gitTest;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("fgg");
+        System.out.println("git change1");
     }
 }
