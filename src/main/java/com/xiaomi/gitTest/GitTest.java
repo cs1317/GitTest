eb249561207e631fe163cd64265c6b9caddce3a7
chengsai1 <chengsai1@xiaomi.com>
2022-09-18 20:50:05 +0800

    master commit before push

--- src/main/java/com/xiaomi/gitTest/GitTest.java
+++ src/main/java/com/xiaomi/gitTest/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("fgg");
         System.out.println("git change1");
         System.out.println("hello hot-fix");
+        System.out.println("push test");
     }
 }
